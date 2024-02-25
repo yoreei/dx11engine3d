0ad761d52cb72bf77ba34ff32c5a826762d07f8e
Danny Grigorov <dgrigoro@cisco.com>
2024-02-25 23:31:54 +0200

    last stage

---
+++ dx11engine3d/source/main.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: main.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "systemclass.h"
+#include <iostream>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	std::cout << "Hello World: " << std::endl;
+	SystemClass* System;
+	bool result;
+	
+	
+	// Create the system object.
+	System = new SystemClass;
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if(result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
